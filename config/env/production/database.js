edeabb425da6c0c85b665c566f0baba05af5e999
Neil McKay <neil.d.mckay@googlemail.com>
2021-03-28 21:51:27 +0100

    changed production database file

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,3 +1,4 @@
+/*
 const { parse } = require("pg-connection-string");
 module.exports = ({ env }) => {
   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
@@ -22,3 +23,29 @@ module.exports = ({ env }) => {
     },
   };
 };
+*/
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+});
